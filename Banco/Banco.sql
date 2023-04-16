0a6a454758b0e807dccdea16186ddbb2926bf742
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-04-16 14:34:10 -0300

    banco

--- Banco/Banco.sql
+++ Banco/Banco.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `db_banco` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_banco`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_banco
 -- ------------------------------------------------------
--- Server version	8.0.28
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -96,7 +98,7 @@ CREATE TABLE `correntista` (
 
 LOCK TABLES `correntista` WRITE;
 /*!40000 ALTER TABLE `correntista` DISABLE KEYS */;
-INSERT INTO `correntista` VALUES (1,'João Silva','11122233344','1990-05-10','senha123'),(2,'Maria Santos','55566677788','1985-01-15','abcd1234'),(3,'Pedro Oliveira','99988877766','2000-11-20','minhasenha');
+INSERT INTO `correntista` VALUES (1,'João Silva','11122233344','1990-05-10',sha1('senha123')),(2,'Maria Santos','55566677788','1985-01-15',sha1('abcd1234')),(3,'Pedro Oliveira','99988877766','2000-11-20',sha1('minhasenha'));
 /*!40000 ALTER TABLE `correntista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-11 15:20:24
+-- Dump completed on 2023-04-16 10:40:35
