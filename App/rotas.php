6953184f84490456b4cccb110729396f72227aac
smokeeaasd <lucas.gomes262@etec.sp.gov.br>
2023-04-16 14:35:14 -0300

    adicionando rotas de contas

--- App/rotas.php
+++ App/rotas.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controller\ContaController;
 use App\Controller\CorrentistaController;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -7,13 +8,55 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 switch ($uri)
 {
 
+	/**
+	 * Rotas para Correntista
+	 */
     case "/api/correntistas":
         CorrentistaController::getCorrentistas();
     break;
 
     case "/api/correntista/by-id":
-        CorrentistaController::getCorrentista();
+        CorrentistaController::getCorrentistaById();
     break;
+
+    case "/api/correntista/new":
+        CorrentistaController::addCorrentista();
+    break;
+
+    case "/api/correntista/update":
+        CorrentistaController::updateCorrentista();
+    break;
+
+    case "/api/correntista/connect":
+        CorrentistaController::getCorrentistaByCPFandSenha();
+    break;
+
+	/**
+	 * Rotas para Conta
+	 */
+	case "/api/contas":
+		ContaController::getContas();
+	break;
+
+	case "/api/conta/by-id":
+		ContaController::getContaById();
+	break;
+
+	case "/api/conta/by-numero":
+		ContaController::getContaByNumero();
+	break;
+
+	case "/api/conta/by-correntista":
+		ContaController::getContaByCorrentista();
+	break;
+
+	case "/api/conta/new":
+		ContaController::addConta();
+	break;
+
+	case "/api/conta/update":
+		ContaController::updateConta();
+	break;
 
     default:
         http_response_code(403);
